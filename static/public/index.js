d5c7dc9c71042c5d94c11c208a096fff31fdcf6a
opal1056 <vishwa.cool05@gmail.com>
2024-08-17 21:21:43 -0400

    Update index.js

--- static/public/index.js
+++ static/public/index.js
@@ -37,8 +37,17 @@ form.addEventListener("submit", async (event) => {
 	let frame = document.getElementById("uv-frame");
 	frame.style.display = "block";
 	let wispUrl = (location.protocol === "https:" ? "wss" : "ws") + "://" + location.host + "/wisp/";
-	if (await connection.getTransport() !== "/epoxy/index.mjs") {
-		await connection.setTransport("/epoxy/index.mjs", [{ wisp: wispUrl }]);
-	}
 	frame.src = __uv$config.prefix + __uv$config.encodeUrl(url);
+	document.getElementById("switcher").onselect = async function (event) {
+    switch (event.target.value) {
+    case "epoxy":
+        await connection.setTransport("/epoxy/index.mjs", [{ wisp: wispUrl }]);
+        break;
+    case "bare":
+        await connection.setTransport("/baremod/index.mjs", [bareUrl]);
+        break;
+    case "libcurl":
+        await connection.setTransport("/libcurl/index.mjs", [{ wisp: wispURL }]);
+        break; 
+}
 });
